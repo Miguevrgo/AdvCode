94f8c7544a3ad3c365699656ea507918fa8f60c5
Miguevrgo <miguevrgo@gmail.com>
2024-12-05 18:29:38 +0100

    Refactor Day01

--- 2024/Day01/src/main.rs
+++ 2024/Day01/src/main.rs
@@ -1,70 +1,31 @@
-use std::fs;
-
-fn part1() {
-    let mut left: Vec<u32> = Vec::new();
-    let mut right: Vec<u32> = Vec::new();
-
-    let contents = fs::read_to_string("./input").unwrap();
-    let strings: Vec<_> = contents.split_whitespace().collect();
-    let elems: Vec<u32> = strings.iter().flat_map(|x| x.parse()).collect();
-
-    for i in (0..elems.len()).step_by(2) {
-        left.push(elems[i]);
-        if i + 1 < elems.len() {
-            right.push(elems[i + 1]);
-        }
-    }
-
-    left.sort();
-    right.sort();
-
-    let total_distance: u32 = left
-        .iter()
-        .zip(right.iter())
-        .map(|(&l, &r)| (l as i32 - r as i32).abs() as u32)
-        .sum();
-
-    println!("Total distance: {}", total_distance);
-}
-
-fn part2() {
-    let mut left: Vec<u32> = Vec::new();
-    let mut right: Vec<u32> = Vec::new();
-
-    let contents = fs::read_to_string("./input").unwrap();
-    let strings: Vec<_> = contents.split_whitespace().collect();
-    let elems: Vec<u32> = strings.iter().flat_map(|x| x.parse()).collect();
-    let mut total_distance = 0;
-
-    for i in (0..elems.len()).step_by(2) {
-        left.push(elems[i]);
-        if i + 1 < elems.len() {
-            right.push(elems[i + 1]);
-        }
-    }
-
-    left.sort();
-    right.sort();
-
-    let mut j = 0;
-
-    for elem in left {
-        let mut counter = 0;
-
-        while j < right.len() && right[j] <= elem {
-            if right[j] == elem {
-                counter += 1;
-            }
-            j += 1;
-        }
-
-        total_distance += counter * elem;
-    }
-
-    println!("Final total distance: {}", total_distance);
-}
+use std::{collections::HashMap, fs};
 
 fn main() {
-    part1();
-    part2();
+    let input = fs::read_to_string("./input").expect("File input not found");
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|l| {
+            let (left, right) = l.split_once(' ').unwrap();
+            (
+                left.trim().parse::<i32>().expect("Unable to read number"),
+                right.trim().parse::<i32>().expect("Unable to read number"),
+            )
+        })
+        .unzip();
+
+    left.sort();
+    right.sort();
+
+    let mut entries = HashMap::new();
+    for &r in &right {
+        *entries.entry(r).or_insert(0) += 1;
+    }
+
+    let p1: i32 = left.iter().zip(&right).map(|(x, y)| (x - y).abs()).sum();
+    let p2: i32 = left
+        .iter()
+        .map(|&r| entries.get(&r).unwrap_or(&0) * r)
+        .sum();
+
+    println!("Part 1: {p1}\nPart 2: {p2}");
 }
