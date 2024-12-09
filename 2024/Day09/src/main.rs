1a165f8bd9684bb8b35e4fc086a4b3d032ca5d33
Miguevrgo <miguevrgo@gmail.com>
2024-12-09 16:29:18 +0100

    Increase efficiency

--- 2024/Day09/src/main.rs
+++ 2024/Day09/src/main.rs
@@ -1,22 +1,47 @@
 use std::fs;
 
-fn solve(mut files: Vec<(usize, i32)>) -> usize {
+fn part1(files: Vec<(usize, i32)>, base: usize) -> usize {
+    let mut files = files;
+    let mut l = 0;
+    let mut r = files.len() - 1;
+
+    while l < r {
+        while l < files.len() && files[l].1 != 0 {
+            l += 1;
+        }
+
+        while r > 0 && files[r].1 == 0 {
+            r -= 1;
+        }
+
+        if l < r {
+            files.swap(l, r);
+            l += 1;
+            r -= 1;
+        }
+    }
+
+    files
+        .iter()
+        .enumerate()
+        .map(|(i, &(_, id))| (i + base) * id as usize)
+        .sum()
+}
+
+fn part2(mut files: Vec<(usize, i32)>, base: usize) -> usize {
     let mut i = files.len() - 1;
     while i > 0 {
         let (size, id) = files[i];
-        if id == -1 {
+        if id == 0 {
             i -= 1;
             continue;
         }
-        if let Some(j) = files[0..i]
-            .iter()
-            .position(|&(s, id)| id == -1 && size <= s)
-        {
+        if let Some(j) = files[0..i].iter().position(|&(s, id)| id == 0 && size <= s) {
             let s = files[j].0;
             files[j] = (size, id);
-            files[i] = (size, -1);
+            files[i] = (size, 0);
             if size < s {
-                files.insert(j + 1, (s - size, -1));
+                files.insert(j + 1, (s - size, 0));
             }
         }
         i -= 1;
@@ -25,7 +50,7 @@ fn solve(mut files: Vec<(usize, i32)>) -> usize {
         .iter()
         .flat_map(|&(s, id)| (0..s).map(move |_| id))
         .enumerate()
-        .map(|(i, id)| if id == -1 { 0 } else { i * id as usize })
+        .map(|(i, id)| (i + base) * id as usize)
         .sum()
 }
 
@@ -33,19 +58,28 @@ fn main() {
     let input = fs::read_to_string("./input").expect("File input not found");
     let mut fs1 = Vec::new();
     let mut fs2 = Vec::new();
-    let mut fid = 0;
-    for (i, ch) in input.chars().filter(|ch| ch.is_ascii_digit()).enumerate() {
+    let mut fid = 1;
+
+    let first_elem = input.chars().next().and_then(|ch| ch.to_digit(10)).unwrap() as usize;
+
+    for (i, ch) in input
+        .chars()
+        .filter(|ch| ch.is_ascii_digit())
+        .enumerate()
+        .skip(1)
+    {
         let v = if i % 2 == 0 {
             fid += 1;
             fid - 1
         } else {
-            -1
+            0
         };
         let value = ch.to_digit(10).unwrap() as usize;
         fs1.extend((0..value).map(|_| (1, v)));
         fs2.push((value, v));
     }
-    let p1 = solve(fs1);
-    let p2 = solve(fs2);
+
+    let p1 = part1(fs1, first_elem);
+    let p2 = part2(fs2, first_elem);
     println!("Part 1: {p1}\nPart 2: {p2}");
 }
