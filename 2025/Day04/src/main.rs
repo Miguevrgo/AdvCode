eda789170c00e429c34ef78213b14453a1d209b6
Miguevrgo <miguevrgo@gmail.com>
2025-12-04 13:00:16 +0100

    Make it fasteeeeeeeer and easier to read

--- 2025/Day04/src/main.rs
+++ 2025/Day04/src/main.rs
@@ -1,71 +1,64 @@
-const PAPER: u8 = 64;
-const DOT: u8 = 46;
+const PAPER: u8 = b'@';
 
-fn count_surrounding(diagram: &[Vec<u8>], pos: (usize, usize)) -> u32 {
-    let mut count: u32 = 0;
+const OFFSETS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
-    for &dr in [-1, 0, 1].iter() {
-        let nr = pos.0 as i32 + dr;
-        for &dc in [-1, 0, 1].iter() {
-            let nc = pos.1 as i32 + dc;
-            if nr >= 0
-                && nc >= 0
-                && nr < diagram.len() as i32
-                && nc < diagram[0].len() as i32
-                && diagram[nr as usize][nc as usize] == PAPER
-            {
-                count += 1;
+fn main() {
+    let input = std::fs::read_to_string("input").expect("File not found");
+    let diagram: Vec<&[u8]> = input.lines().map(str::as_bytes).collect();
+    let (h, w) = (diagram.len(), diagram[0].len());
+    let mut padded = vec![vec![u8::MAX; w + 2]; h + 2];
+
+    let mut changes = Vec::with_capacity(h * w);
+    for r in 0..h {
+        for c in 0..w {
+            if diagram[r][c] != PAPER {
+                continue;
+            }
+
+            let neighbours = OFFSETS
+                .iter()
+                .filter(|&&(dr, dc)| {
+                    let nr = r as isize + dr;
+                    let nc = c as isize + dc;
+                    nr >= 0
+                        && nr < h as isize
+                        && nc >= 0
+                        && nc < w as isize
+                        && diagram[nr as usize][nc as usize] == PAPER
+                })
+                .count();
+
+            if neighbours < 4 {
+                changes.push((r + 1, c + 1));
+            } else {
+                padded[r + 1][c + 1] = neighbours as u8;
             }
         }
     }
 
-    count - 1 // Self
-}
-
-fn main() {
-    let input = std::fs::read_to_string("input").expect("File not found");
-    let diagram: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_vec()).collect();
-
-    let p1: u32 = diagram.iter().enumerate().fold(0, |acc, (line_n, line)| {
-        let count = line
-            .iter()
-            .enumerate()
-            .filter(|&(pos, &cont)| cont == PAPER && count_surrounding(&diagram, (line_n, pos)) < 4)
-            .count();
-        acc + count as u32
-    });
-
+    let p1 = changes.len();
     let mut p2 = 0;
-    let mut grid = diagram.clone();
-    loop {
-        let mut updated_positions = Vec::new();
-        let count2 = grid.iter().enumerate().fold(0, |acc, (line_n, line)| {
-            let count = line
-                .iter()
-                .enumerate()
-                .filter(|&(pos, &cont)| {
-                    cont == PAPER && count_surrounding(&grid, (line_n, pos)) < 4
-                })
-                .count();
 
-            updated_positions.extend(
-                line.iter()
-                    .enumerate()
-                    .filter(|&(pos, &cont)| {
-                        cont == PAPER && count_surrounding(&grid, (line_n, pos)) < 4
-                    })
-                    .map(|(pos, _)| (line_n, pos)),
-            );
+    while let Some((r, c)) = changes.pop() {
+        p2 += 1;
+        OFFSETS.iter().for_each(|&(dr, dc)| {
+            let nr = (r as isize + dr) as usize;
+            let nc = (c as isize + dc) as usize;
+            padded[nr][nc] -= 1;
 
-            acc + count
+            if padded[nr][nc] == 4 {
+                changes.push((nr, nc));
+            }
         });
-        if count2 == 0 {
-            break;
-        }
-        for (line_n, pos) in updated_positions {
-            grid[line_n][pos] = DOT;
-        }
-        p2 += count2;
     }
 
     println!("Part 1: {p1}, Part 2: {p2}")
