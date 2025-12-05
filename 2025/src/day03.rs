9ddec92d203709b3186dc6ac5f370bf5a9be686b
Miguevrgo <miguevrgo@gmail.com>
2025-12-05 13:51:27 +0100

    Improve Day03 efficiency

--- 2025/src/day03.rs
+++ 2025/src/day03.rs
@@ -1,22 +1,26 @@
+use std::mem::replace;
 pub fn run() {
     let input = include_str!("../input/day03.txt");
     let banks: Vec<&[u8]> = input.lines().map(str::as_bytes).collect();
 
-    let (p1, p2): (u64, u64) = banks.iter().fold((0, 0), |(acc1, acc2), &bank| {
+    let (p1, p2): (u64, u64) = banks.iter().fold((0, 0), |(acc1, acc2), bank| {
         let solve = |tops: &mut [u8]| -> u64 {
-            for (i, &rating) in bank.iter().enumerate() {
-                let mut pos = tops.len().saturating_sub(bank.len() - i);
+            let mut final_batteries = vec![0; tops.len()];
+            let wall = bank.len() - tops.len();
+            final_batteries.copy_from_slice(&bank[wall..]);
 
-                while pos < tops.len() {
-                    if rating > tops[pos] {
-                        tops[pos] = rating;
-                        tops[(pos + 1)..].fill(0);
+            for mut rating in bank[0..wall].iter().rev().copied() {
+                for battery in &mut final_batteries {
+                    if rating < *battery {
                         break;
                     }
-                    pos += 1;
+
+                    rating = replace(battery, rating);
                 }
             }
-            tops.iter()
+
+            final_batteries
+                .iter()
                 .fold(0, |acc, &d| acc * 10 + d.saturating_sub(b'0') as u64)
         };
 
