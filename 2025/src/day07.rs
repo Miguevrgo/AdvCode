42f5052940925f09cb1186f802089c132d0b7b3c
Miguevrgo <miguevrgo@gmail.com>
2025-12-07 09:22:09 +0100

    Easy solution for day07, efficient one coming soon

--- 2025/src/day07.rs
+++ 2025/src/day07.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+fn dfs(
+    row: usize,
+    col: usize,
+    grid: &Vec<&[u8]>,
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if row == grid.len() - 1 {
+        return 1;
+    }
+
+    if let Some(&cached) = cache.get(&(row, col)) {
+        cached
+    } else if grid[row + 1][col] == b'^' {
+        let timelines = dfs(row + 1, col - 1, grid, cache) + dfs(row + 1, col + 1, grid, cache);
+        cache.insert((row, col), timelines);
+        timelines
+    } else {
+        let timelines = dfs(row + 1, col, grid, cache);
+        cache.insert((row, col), timelines);
+        timelines
+    }
+}
+
 pub fn run() {
     let input = include_str!("../input/day07.txt");
     let start = (0, input.find('S').unwrap());
@@ -22,5 +47,9 @@ pub fn run() {
         acc + count
     });
 
-    println!("Part 1: {p1}");
+    let mut cache = std::collections::HashMap::new();
+    let grid: Vec<&[u8]> = input.lines().map(str::as_bytes).collect();
+    let p2 = dfs(start.0, start.1, &grid, &mut cache);
+
+    println!("Part 1: {p1} Part 2: {p2}");
 }
